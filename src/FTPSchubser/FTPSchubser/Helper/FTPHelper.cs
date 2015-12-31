e784ae94430f0987e2bc23caf15a99ccd7d35b4a
farfelu <git@farfelu.eu>
2015-12-31 16:35:52 +0100

    add helper files

---
+++ src/FTPSchubser/FTPSchubser/Helper/FTPHelper.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPSchubser.Helper
+{
+    class FTPHelper
+    {
+        public string Host { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Path { get; private set; }
+        public int Port { get; private set; }
+        public bool Pasv { get; private set; }
+
+        public FTPHelper(string host, string username, string password, string path, int port = 21, bool pasv = true)
+        {
+            Host = host;
+            Username = username;
+            Password = password;
+            Path = path;
+            Port = port;
+            Pasv = pasv;
+        }
+
+        public async Task<bool> FileExistsAsync(string fileName)
+        {
+            return false;
+        }
+
+        public async Task UploadFilesAsync(IEnumerable<string> files, IProgress<FTPProgress> progress = null)
+        {
+            var fileInfos = new List<FileInfo>();
+
+            foreach (var file in files)
+            {
+                if (File.Exists(file))
+                {
+                    fileInfos.Add(new FileInfo(file));
+                }
+            }
+
+            var bytesTotal = fileInfos.Sum(x => x.Length);
+            var bytesDone = 0;
+            var filesTotal = fileInfos.Count;
+            var filesDone = 0;
+
+
+            foreach (var fileInfo in fileInfos)
+            {
+                var fileName = System.IO.Path.GetFileName(fileInfo.FullName);
+                var ftpUrl = Utils.FormatFTPUrl(Host, Path, fileName, Port);
+
+                // from https://msdn.microsoft.com/en-us/library/ms229715%28v=vs.110%29.aspx
+                // Get the object used to communicate with the server.
+                var request = (FtpWebRequest)WebRequest.Create(ftpUrl);
+
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.UsePassive = Pasv;
+                request.Credentials = new NetworkCredential(Username, Password);
+
+                using (var fileStream = File.OpenRead(fileInfo.FullName))
+                using (var requestStream = await request.GetRequestStreamAsync())
+                {
+                    var buffer = new byte[1024 * 4]; // 4KiB cache
+                    var readBytesCount = 0;
+
+
+                    //used so we can measure how many bytes per second we transfer
+                    var watch = new Stopwatch();
+                    watch.Start();
+
+                    while ((readBytesCount = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await requestStream.WriteAsync(buffer, 0, readBytesCount);
+                        bytesDone += readBytesCount;
+
+                        if (progress != null)
+                        {
+                            progress.Report(new FTPProgress(filesTotal, filesDone, bytesTotal, bytesDone, readBytesCount / watch.ElapsedMilliseconds));
+                        }
+                    }
+                }
+
+                filesDone++;
+            }
+        }
+
+        public class FTPProgress
+        {
+            public int FilesTotal { get; }
+            public int FilesDone { get; }
+            public long BytesTotal { get; }
+            public long BytesDone { get; }
+            public float BytesPercent
+            {
+                get
+                {
+                    return BytesTotal == 0 ? 0 : (BytesDone / BytesTotal);
+                }
+            }
+
+            public string TransferSpeed { get; }
+
+            public FTPProgress(int filesTotal, int filesDone, long bytesTotal, long bytesDone, long bytesPerSecond = -1)
+            {
+                FilesTotal = filesTotal;
+                FilesDone = filesDone;
+                BytesTotal = bytesTotal;
+                BytesDone = bytesDone;
+
+                if (bytesPerSecond >= 0)
+                {
+                    TransferSpeed = $"{Utils.BytesToString(bytesPerSecond)}/s";
+                }
+            }
+        }
+    }
+}
---
+++ src/FTPSchubser/FTPSchubser/Helper/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPSchubser.Helper
+{
+    class Utils
+    {
+
+        private static string FormatUrl(string protocol, string host, string path, string fileName, int? port = null)
+        {
+            return $"{protocol}://{host}{(port == null ? "" : ":" + port)}{(path == null ? "" : "/" + path)}{(fileName == null ? "" : "/" + fileName)}";
+        }
+
+        public static string FormatFTPUrl(string host, string path = null, string fileName = null, int port = 21)
+        {
+            return FormatUrl("ftp", host, path, fileName, port);
+        }
+
+        public static string FormatHTTPUrl(string host, string path, string fileName)
+        {
+            return FormatUrl("http", host, path, fileName);
+        }
+
+        // from http://stackoverflow.com/a/4975942
+        public static string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+    }
+}
