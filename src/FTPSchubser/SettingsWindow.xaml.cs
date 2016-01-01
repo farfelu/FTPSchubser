34bc65503ed4b4670dcb674ac08d18cf7991d23a
farfelu <git@farfelu.eu>
2016-01-01 17:54:46 +0100

    basic settings window

--- src/FTPSchubser/SettingsWindow.xaml.cs
+++ src/FTPSchubser/SettingsWindow.xaml.cs
@@ -10,25 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FTPSchubser
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SettingsWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SettingsWindow : Window
     {
-        public MainWindow()
+        public SettingsWindow(Window owner)
         {
+            this.Owner = owner;
             InitializeComponent();
         }
 
-        private void btn_settings_Click(object sender, RoutedEventArgs e)
+        private void btn_save_Click(object sender, RoutedEventArgs e)
         {
-            var settingsWindow = new SettingsWindow(this);
-            settingsWindow.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
