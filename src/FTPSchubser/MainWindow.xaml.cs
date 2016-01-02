2132f58402b1644aa47b49875f4d9299296c1d1f
farfelu <git@farfelu.eu>
2016-01-02 12:28:03 +0100

    add some really bad exception handling

--- src/FTPSchubser/MainWindow.xaml.cs
+++ src/FTPSchubser/MainWindow.xaml.cs
@@ -42,7 +42,7 @@ namespace FTPSchubser
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                UploadFilesAsync(files);       
+                UploadFilesAsync(files);
             }
         }
 
@@ -56,77 +56,86 @@ namespace FTPSchubser
 
         private async Task UploadFilesAsync(IEnumerable<string> files, bool fromCommandline = false)
         {
-            resetProgressBar();
-            var ftp = new FTPHelper(App.Settings.Host, App.Settings.User, App.Settings.Password, App.Settings.ServerPath, App.Settings.Port, App.Settings.PassiveMode);
-
-            // create file and directory list
-            var fileList = files.OrderBy(x => x).ToList();
-            var dirList = fileList.Where(x => Utils.IsDirectory(x)).ToList();
-
-            // remove directories from filelist
-            fileList.RemoveAll(x => dirList.Contains(x));
-
-            var uploadList = new List<UploadFile>(fileList.Select(x => new UploadFile(x)));
-
-            if (dirList.Count > 0)
+            try
             {
+                resetProgressBar();
                 progress_bar.IsIndeterminate = true;
-                var zippedFiles = await ZIPHelper.ZipDirectoriesAsync(dirList, new Progress<ZIPHelper.ZIPProgress>((x =>
+                var ftp = new FTPHelper(App.Settings.Host, App.Settings.User, App.Settings.Password, App.Settings.ServerPath, App.Settings.Port, App.Settings.PassiveMode);
+
+                // create file and directory list
+                var fileList = files.OrderBy(x => x).ToList();
+                var dirList = fileList.Where(x => Utils.IsDirectory(x)).ToList();
+
+                // remove directories from filelist
+                fileList.RemoveAll(x => dirList.Contains(x));
+
+                var uploadList = new List<UploadFile>(fileList.Select(x => new UploadFile(x)));
+
+                if (dirList.Count > 0)
+                {
+                    var zippedFiles = await ZIPHelper.ZipDirectoriesAsync(dirList, new Progress<ZIPHelper.ZIPProgress>((x =>
+                    {
+                        progress_bar.IsIndeterminate = false;
+                        progress_bar.Maximum = x.BytesTotal;
+                        progress_bar.Value = x.BytesDone;
+                        progress_label.Content = "ZIP: " + x.ToString();
+                    })));
+                    uploadList.AddRange(zippedFiles);
+                }
+
+                progress_bar.IsIndeterminate = true;
+
+                uploadList = uploadList.OrderBy(x => x.FileInfo.Name).ToList();
+
+                if (App.Settings.CheckOverwrite)
+                {
+                    var existingFiles = (await ftp.GetExistingFiles(uploadList)).ToList();
+                    if (existingFiles.Any())
+                    {
+                        var result = MessageBox.Show(this, "The following files will be overwritten:\r\n" + string.Join("\r\n", existingFiles.Select(x => x.FileInfo.Name)), "Confirm overwrite", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                        if (result != MessageBoxResult.Yes)
+                        {
+                            // remove the files from our list if we don't want to overwrite them
+                            uploadList.RemoveAll(x => existingFiles.Contains(x));
+                        }
+                    }
+                }
+
+                var uploadedFiles = await ftp.UploadFilesAsync(uploadList, new Progress<Helper.FTPHelper.FTPProgress>((x =>
                 {
                     progress_bar.IsIndeterminate = false;
                     progress_bar.Maximum = x.BytesTotal;
                     progress_bar.Value = x.BytesDone;
-                    progress_label.Content = "ZIP: " + x.ToString();
+                    progress_label.Content = "Upload: " + x.ToString();
                 })));
-                uploadList.AddRange(zippedFiles);
-                progress_bar.IsIndeterminate = false;
-            }
 
-            uploadList = uploadList.OrderBy(x => x.FileInfo.Name).ToList();
-
-            if (App.Settings.CheckOverwrite)
-            {
-                progress_bar.IsIndeterminate = true;
-                var existingFiles = (await ftp.GetExistingFiles(uploadList)).ToList();
-                if (existingFiles.Any())
+                if (App.Settings.CopyClipboard)
                 {
-                    var result = MessageBox.Show(this, "The following files will be overwritten:\r\n" + string.Join("\r\n", existingFiles.Select(x => x.FileInfo.Name)), "Confirm overwrite", MessageBoxButton.YesNo, MessageBoxImage.Information);
-                    if (result != MessageBoxResult.Yes)
+                    var urls = uploadedFiles.Select(x => Utils.FormatHTTPUrl(App.Settings.UrlPath, App.Settings.Host, App.Settings.ServerPath, x.UploadName));
+
+                    if (App.Settings.Minify)
                     {
-                        // remove the files from our list if we don't want to overwrite them
-                        uploadList.RemoveAll(x => existingFiles.Contains(x));
+                        progress_label.Content = "Minifying links...";
+
+                        urls = await URLHelper.ShortenUrlsAsync(urls);
                     }
+
+                    // always add newline at the end
+                    Clipboard.SetText(string.Join("\r\n", urls) + "\r\n");
                 }
-                progress_bar.IsIndeterminate = false;
-            }
 
-            var uploadedFiles = await ftp.UploadFilesAsync(uploadList, new Progress<Helper.FTPHelper.FTPProgress>((x =>
-            {
-                progress_bar.Maximum = x.BytesTotal;
-                progress_bar.Value = x.BytesDone;
-                progress_label.Content = "Upload: " + x.ToString();
-            })));
-
-            if (App.Settings.CopyClipboard)
-            {
-                var urls = uploadedFiles.Select(x => Utils.FormatHTTPUrl(App.Settings.UrlPath, App.Settings.Host, App.Settings.ServerPath, x.UploadName));
-
-                if (App.Settings.Minify)
+                // clean up temp files
+                foreach (var uf in uploadedFiles.Where(x => x.IsTemp))
                 {
-                    urls = await URLHelper.ShortenUrlsAsync(urls);
+                    System.IO.File.Delete(uf.FilePath);
                 }
 
-                // always add newline at the end
-                Clipboard.SetText(string.Join("\r\n", urls) + "\r\n");
+                resetProgressBar();
             }
-
-            // clean up temp files
-            foreach (var uf in uploadedFiles.Where(x => x.IsTemp))
+            catch (Exception ex)
             {
-                System.IO.File.Delete(uf.FilePath);
-            }            
-
-            resetProgressBar();
+                MessageBox.Show(this, "I screwed up.\r\n" + ex.Message + "\r\n" + ex.StackTrace, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
             if (fromCommandline && !App.Settings.KeepWindowOpen)
             {
