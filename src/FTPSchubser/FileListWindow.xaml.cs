d3a78e74d3723998703178cbfc0d4e031e4cc08f
farfelu <git@farfelu.eu>
2016-01-03 20:15:17 +0100

    very basic filelist

--- src/FTPSchubser/FileListWindow.xaml.cs
+++ src/FTPSchubser/FileListWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FTPSchubser.Helper;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,23 @@ namespace FTPSchubser
         {
             this.Owner = owner;
             InitializeComponent();
+
+            FillDatagrid();
+        }
+
+        private async Task FillDatagrid()
+        {
+            var ftp = new FTPHelper(App.Settings.Host, App.Settings.User, App.Settings.Password, App.Settings.ServerPath, App.Settings.Port, App.Settings.PassiveMode);
+            var files = await ftp.ListFilesAsync();
+            
+            gridFiles.ItemsSource = files.Select(x => new
+            {
+                x.IsDirectory,
+                x.Timestamp,
+                Size = Utils.BytesToString(x.Size),
+                x.Filename,
+                URL = Utils.FormatHTTPUrl(App.Settings.UrlPath, App.Settings.Host, App.Settings.ServerPath, x.Filename)
+            });
         }
     }
 }
