510d0e18f9419aae0a29e655f9b2ad9aee5a90aa
farfelu <git@farfelu.eu>
2016-01-03 22:27:15 +0100

    [WIP] filelist

--- src/FTPSchubser/FileListWindow.xaml.cs
+++ src/FTPSchubser/FileListWindow.xaml.cs
@@ -29,19 +29,60 @@ namespace FTPSchubser
             FillDatagrid();
         }
 
+        private IEnumerable<FTPFileListItem> Files { get; set; }
+
         private async Task FillDatagrid()
         {
             var ftp = new FTPHelper(App.Settings.Host, App.Settings.User, App.Settings.Password, App.Settings.ServerPath, App.Settings.Port, App.Settings.PassiveMode);
-            var files = await ftp.ListFilesAsync();
-            
-            gridFiles.ItemsSource = files.Select(x => new
+            var files = (await ftp.ListFilesAsync())
+                        .Where(x => !x.IsDirectory)
+                        .OrderByDescending(x => x.Timestamp)
+                        .Select(x => new FTPFileListItem()
             {
-                x.IsDirectory,
-                x.Timestamp,
-                Size = Utils.BytesToString(x.Size),
-                x.Filename,
-                URL = Utils.FormatHTTPUrl(App.Settings.UrlPath, App.Settings.Host, App.Settings.ServerPath, x.Filename)
+                Timestamp = x.Timestamp,
+                Size = x.Size,
+                SizeReadable = Utils.BytesToString(x.Size),
+                Filename = x.Filename,
+                Url = Utils.FormatHTTPUrl(App.Settings.UrlPath, App.Settings.Host, App.Settings.ServerPath, x.Filename)
             });
+            Files = new ObservableCollection<FTPFileListItem>(files);
+            gridFiles.ItemsSource = Files;
+
+            gridFiles.Visibility = Visibility.Visible;
+            progress.Visibility = Visibility.Hidden;
+            this.Cursor = null;
+        }
+
+        private void btnListcopy_Click(object sender, RoutedEventArgs e)
+        {
+            var obj = ((FrameworkElement)sender).DataContext as FTPFileListItem;
+            System.Diagnostics.Process.Start(obj.Url);
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            var checkedFiles = Files.Where(x => x.ToCopy).ToList();
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private class FTPFileListItem
+        {
+            public DateTime? Timestamp { get; set; }
+            public long Size { get; set; }
+            public string SizeReadable { get; set; }
+            public string Filename { get; set; }
+            public string Url { get; set; }
+            public bool ToCopy { get; set; }
+            public bool ToDelete { get; set; }
         }
     }
 }
