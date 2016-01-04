2f843d9cc4ccdb8837646d77e768e492870f5eb8
farfelu <git@farfelu.eu>
2016-01-04 18:15:17 +0100

    add copy links

--- src/FTPSchubser/FileListWindow.xaml.cs
+++ src/FTPSchubser/FileListWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,10 +31,40 @@ namespace FTPSchubser
             FillDatagrid();
         }
 
-        private IEnumerable<FTPFileListItem> Files { get; set; }
+        private ObservableCollection<FTPFileListItem> Files { get; set; }
+
+        private void ShowProgress(string text = null)
+        {
+            gridFiles.Visibility = Visibility.Hidden;
+            progress_bar.Visibility = Visibility.Visible;
+            progress_label.Visibility = Visibility.Visible;
+            progress_label.Content = text;
+            this.Cursor = Cursors.Wait;
+
+            btnCopy.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnRefresh.IsEnabled = false;
+        }
+
+        private void HideProgress()
+        {
+            gridFiles.Visibility = Visibility.Visible;
+            progress_bar.Visibility = Visibility.Hidden;
+            progress_label.Visibility = Visibility.Hidden;
+            progress_label.Content = null;
+            this.Cursor = null;
+
+            btnCopy.IsEnabled = true;
+            btnDelete.IsEnabled = true;
+            btnRefresh.IsEnabled = true;
+        }
 
         private async Task FillDatagrid()
         {
+            ShowProgress("looking up files");
+
+            Files = null;
+
             var ftp = new FTPHelper(App.Settings.Host, App.Settings.User, App.Settings.Password, App.Settings.ServerPath, App.Settings.Port, App.Settings.PassiveMode);
             var files = (await ftp.ListFilesAsync())
                         .Where(x => !x.IsDirectory)
@@ -48,9 +80,7 @@ namespace FTPSchubser
             Files = new ObservableCollection<FTPFileListItem>(files);
             gridFiles.ItemsSource = Files;
 
-            gridFiles.Visibility = Visibility.Visible;
-            progress.Visibility = Visibility.Hidden;
-            this.Cursor = null;
+            HideProgress();
         }
 
         private void btnListcopy_Click(object sender, RoutedEventArgs e)
@@ -59,14 +89,40 @@ namespace FTPSchubser
             System.Diagnostics.Process.Start(obj.Url);
         }
 
-        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        private async void btnCopy_Click(object sender, RoutedEventArgs e)
         {
-            var checkedFiles = Files.Where(x => x.ToCopy).ToList();
+            if (!Files.Any(x => x.ToCopy))
+            {
+                return;
+            }
+
+            var files = Files.Where(x => x.ToCopy);
+            
+            var urls = files.Select(x => x.Url);
+
+            if (App.Settings.Minify)
+            {
+                ShowProgress("minifying links");
+
+                urls = await URLHelper.ShortenUrlsAsync(urls);
+
+                HideProgress();
+            }
+
+            // always add newline at the end
+            Clipboard.SetText(string.Join("\r\n", urls) + "\r\n");
+
+            //uncheck the copied files
+            foreach (var file in files)
+            {
+                file.ToCopy = false;
+            }
+            
         }
 
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
-
+            FillDatagrid();
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
@@ -74,15 +130,41 @@ namespace FTPSchubser
 
         }
 
-        private class FTPFileListItem
+        private class FTPFileListItem: INotifyPropertyChanged
         {
             public DateTime? Timestamp { get; set; }
             public long Size { get; set; }
             public string SizeReadable { get; set; }
             public string Filename { get; set; }
             public string Url { get; set; }
-            public bool ToCopy { get; set; }
-            public bool ToDelete { get; set; }
+
+            private bool toCopy = false;
+            public bool ToCopy
+            {
+                get { return toCopy; }
+                set { SetField(ref toCopy, value); }
+            }
+
+            private bool toDelete = false;
+            public bool ToDelete
+            {
+                get { return toDelete; }
+                set { SetField(ref toDelete, value); }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+            {
+                if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+                field = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+
         }
     }
 }
